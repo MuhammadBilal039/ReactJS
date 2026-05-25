2f5f7dc6b5ada1d1e9a41a0d4c42a10bcbd2ff40
MuhammadBilal039 <muhammad.bilal2021pk@gmail.com>
2026-05-25 11:13:37 +0500

    Make a Simple Steps Counter With Date Changing

---
+++ Steps Counter with Date Changing/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [steps, setSteps] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const days = ["Sun", "Mon", "Tues", "Thur", "Fri", "Sat"];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const date = new Date();
+  const setDate = date.setDate(count);
+
+  console.log(date);
+
+  return (
+    <div className="container">
+      <h2 className="heading">Steps Counter With Date Changing</h2>
+      <h3>
+        <button onClick={() => (steps >= 2 ? setSteps(steps - 1) : null)}>
+          -
+        </button>
+        Step: {steps}
+        <button onClick={() => setSteps(steps + 1)}>+</button>
+      </h3>
+      <h3>
+        <button onClick={() => setCount(steps - 1)}>-</button>
+        Counter: {count}
+        <button onClick={() => setCount(count + steps)}>+</button>
+      </h3>
+
+      <h2>
+        Today is {days[date.getDay()]} {months[date.getMonth()]}{" "}
+        {date.getDate(setDate)} {date.getFullYear()}
+      </h2>
+    </div>
+  );
+}
+
+export default App;
